058cf35564723d6652706b9db7484dc65656e5c1
sugarboo <kennethbaak@gmail.com>
2024-07-28 16:34:30 +0800

    feat: easy/00013-Roman-to-Integer

--- src/code/easy/00013-Roman-to-Integer.ts
+++ src/code/easy/00013-Roman-to-Integer.ts
@@ -38,8 +38,79 @@ function romanToInt1(s: string): number {
   return total
 }
 
+/**
+ * Solution 2: Iterative Approach
+ * 
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ * 
+ * @param s 
+ * @returns 
+ */
+function romanToInt2(s: string): number {
+  const map = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  }
+
+  let total = 0, prev = 0
+  for (let i = 0; i < s.length; i++) {
+    const curr = map[s[i]]
+    // If current value is greater than the previous value, subtract twice the previous value; Otherwise, add current value to the sum
+    total += curr > prev ? curr - 2 * prev : curr
+    // Update the previous value for the next iteration
+    prev = curr
+  }
+
+  return total
+}
+
+/**
+ * Solution 3: Using Map for Flexibility
+ * 
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ * 
+ * @param s 
+ * @returns 
+ */
+function romanToInt3(s: string): number {
+  const roman = new Map([
+    ['I', 1],
+    ['V', 5],
+    ['X', 10],
+    ['L', 50],
+    ['C', 100],
+    ['D', 500],
+    ['M', 1000],
+  ])
+
+  let total = 0, prev = 0
+
+  for (let i = 0; i < s.length; i++) {
+    const curr = roman.get(s[i])!
+    // If the current value is greater than the previous value, subtract the previous value; Otherwise, add the previous value
+    if (curr > prev) {
+      total -= prev
+    } else {
+      total += prev
+    }
+    // Update the previous value for next iteration
+    prev = curr
+  }
+  // add the last value to sum
+  total += prev
+
+  return total
+}
+
 export function romanToInt(s: string): number {
-  return romanToInt1(s)
+  return romanToInt3(s)
 }
 
 /**
