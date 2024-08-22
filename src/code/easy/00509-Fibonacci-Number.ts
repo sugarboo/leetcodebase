8c728e23d570f6e73d924e15c278fd266e771e45
sugarboo <kennethbaak@gmail.com>
2024-08-22 11:34:02 +0800

    feat: easy/00509-Fibonacci-Number

--- src/code/easy/00509-Fibonacci-Number.ts
+++ src/code/easy/00509-Fibonacci-Number.ts
@@ -1,4 +1,4 @@
-export function fib(n: number): number {
+function fibRecursively(n: number): number {
   if (n === 0) {
     return 0
   }
@@ -6,4 +6,19 @@ export function fib(n: number): number {
     return 1
   }
   return fib(n - 1) + fib(n - 2)
+}
+
+function fibDp(n: number): number {
+  const dp: number[] = [0, 1]
+
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2]
+  }
+
+  return dp[n]
+}
+
+export function fib(n: number): number {
+  // return fibRecursively(n)
+  return fibDp(n)
 }
