f7a0eb69619fff2b538893896160abe3628aa6a3
sugarboo <kennethbaak@gmail.com>
2024-09-17 18:45:03 +0800

    feat: easy/00231-Power-of-Two

---
+++ src/code/easy/00231-Power-of-Two.ts
@@ -0,0 +1,3 @@
+export function isPowerOfTwo(n: number): boolean {
+  return Number.isInteger(Math.log2(n))
+}
---
+++ src/test/easy/00231-Power-of-Two.spec.ts
@@ -0,0 +1,27 @@
+import { describe, expect, it } from 'vitest'
+import { summaryRanges } from '../../code/easy/00228-Summary-Ranges'
+import { isPowerOfTwo } from '../../code/easy/00231-Power-of-Two'
+
+describe('power of two test case 🥇', () => {
+  it('should return the expected result', () => {
+    expect(isPowerOfTwo(1)).toBe(true)
+  })
+})
+
+describe('power of two test case 🥈', () => {
+  it('should return the expected result', () => {
+    expect(isPowerOfTwo(16)).toBe(true)
+  })
+})
+
+describe('power of two test case 🥉', () => {
+  it('should return the expected result', () => {
+    expect(isPowerOfTwo(3)).toBe(false)
+  })
+})
+
+describe('power of two test case 🏅', () => {
+  it('should return the expected result', () => {
+    expect(isPowerOfTwo(8)).toBe(true)
+  })
+})
