d3e0264b7c927177a6035d8d8c52949d750934bc
sugarboo <kennethbaak@gmail.com>
2025-08-13 08:55:54 +0800

    fix: easy/00326-Power-of-Three

--- src/code/easy/00326-Power-of-Three.ts
+++ src/code/easy/00326-Power-of-Three.ts
@@ -1,3 +1,15 @@
-export function isPowerOfThree(n: number): boolean {
+export function isPowerOfThree1(n: number): boolean {
+  while (n !== 0 && n % 3 === 0) {
+    n = Math.floor(n / 3)
+  }
+
+  return n === 1
+}
+
+export function isPowerOfThree2(n: number): boolean {
   return (Math.log10(n) / Math.log10(3)) % 1 === 0
+}
+
+export function isPowerOfThree(n: number): boolean {
+  return isPowerOfThree1(n)
 }
