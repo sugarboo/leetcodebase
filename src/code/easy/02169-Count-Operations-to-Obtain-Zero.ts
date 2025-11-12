286976a5f165cd974bc9975c7fdbc770bfb5aeee
sugarboo <kennethbaak@gmail.com>
2025-11-12 09:06:07 +0800

    refactor: easy/02169-Count-Operations-to-Obtain-Zero

--- src/code/easy/02169-Count-Operations-to-Obtain-Zero.ts
+++ src/code/easy/02169-Count-Operations-to-Obtain-Zero.ts
@@ -1,4 +1,4 @@
-export function countOperations(num1: number, num2: number): number {
+export function countOperations1(num1: number, num2: number): number {
   let cnt = 0
   while (num1 !== 0 && num2 !== 0) {
     [num1, num2] = [
@@ -8,4 +8,17 @@ export function countOperations(num1: number, num2: number): number {
     cnt++
   }
   return cnt
+}
+
+function countOperations2(num1: number, num2: number): number {
+  let ans = 0
+  while (num2 > 0) {
+    ans += Math.floor(num1 / num2);
+    [num1, num2] = [num2, num1 % num2]
+  }
+  return ans
+}
+
+export function countOperations(num1: number, num2: number): number {
+  return countOperations2(num1, num2)
 }
