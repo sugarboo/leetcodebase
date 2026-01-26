85ae47bfa1400d052ef862f2ddb377484a046c9a
sugarboo <kennethbaak@gmail.com>
2026-01-26 09:12:32 +0800

    fix: easy/01200-Minimum-Absolute-Difference

--- src/code/easy/01200-Minimum-Absolute-Difference.ts
+++ src/code/easy/01200-Minimum-Absolute-Difference.ts
@@ -1,17 +1,18 @@
 export function minimumAbsDifference(arr: number[]): number[][] {
+  const ans: number[][] = []
   arr.sort((a, b) => a - b)
-  const result: number[][] = []
   let minDiff = Infinity
   for (let i = 0; i < arr.length - 1; i++) {
-    const diff = arr[i + 1] - arr[i]
+    const x = arr[i]
+    const y = arr[i + 1]
+    const diff = y - x
     if (diff < minDiff) {
+      ans.length = 0
       minDiff = diff
+      ans.push([x, y])
+    } else if (diff === minDiff) {
+      ans.push([x, y])
     }
   }
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i + 1] - arr[i] === minDiff) {
-      result.push([arr[i], arr[i + 1]])
-    }
-  }
-  return result
+  return ans
 }
