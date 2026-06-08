139f81d8cb16e032620524e2f3bd59942f944ce3
sugarboo <kennethbaak@gmail.com>
2026-06-08 09:44:55 +0800

    fix: medium/02161-partition-array-according-to-given-pivot

--- src/code/medium/02161-Partition-Array-According-to-Given-Pivot.ts
+++ src/code/medium/02161-Partition-Array-According-to-Given-Pivot.ts
@@ -1,16 +1,16 @@
 export function pivotArray(nums: number[], pivot: number): number[] {
   const lefts: number[] = []
+  const pivots: number[] = []
   const rights: number[] = []
 
   for (const num of nums) {
     if (num < pivot) {
       lefts.push(num)
     } else if (num === pivot) {
-      rights.unshift(num)
+      pivots.push(num)
     } else {
       rights.push(num)
     }
   }
-
-  return [...lefts, ...rights]
+  return [...lefts, ...pivots, ...rights]
 }
